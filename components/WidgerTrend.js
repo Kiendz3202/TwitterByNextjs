f64f089847b197090d3cfac9037eee0b99eaf948
Kiendz3202 <73543426+Kiendz3202@users.noreply.github.com>
2021-12-23 11:58:31 +0700

    ok

---
+++ components/WidgerTrend.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+function WidgerTrend({ trendDetail }) {
+    return (
+        <div className='flex justify-between items-center  px-[12px] py-[8px] hover:bg-zinc-700 cursor-pointer'>
+            <div>
+                <p className='text-[13px] font-light text-gray-400'>{trendDetail.heading}</p>
+                <p className=' text-[15px] font-semibold '>{trendDetail.description}</p>
+                <p className='text-[13px] font-light text-gray-400'>Trending with {trendDetail.tags.map((tag, index) => <span key={index}>{tag}</span>)}</p>
+            </div>
+            <div>
+                <MoreHorizIcon />
+            </div>
+        </div>
+    )
+}
+
+export default WidgerTrend
